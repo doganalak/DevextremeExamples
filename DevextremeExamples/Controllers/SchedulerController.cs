c9512437c5cfaed01976462c9a7202b53dae5b94
doğan alak <doganalak@msn.com>
2022-03-16 21:55:26 +0300

    Scheduler

---
+++ DevextremeExamples/Controllers/SchedulerController.cs
@@ -0,0 +1,31 @@
+﻿using DevextremeExamples.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DevextremeExamples.Controllers
+{
+    public class SchedulerController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public SchedulerController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            var rootPath = _hostingEnvironment.WebRootPath;
+            var fullPath = Path.Combine(rootPath, "JsonFile/Data.json");
+            var jsonData = System.IO.File.ReadAllText(fullPath);
+            List<SchedulerDto> testDtos = JsonConvert.DeserializeObject<List<SchedulerDto>>(jsonData);
+            return View(testDtos);
+        }
+        [HttpPost]
+        public IActionResult Update(string models)
+        {
+            return Json(false);
+        }
+    }
+}
---
+++ DevextremeExamples/Models/SchedulerDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DevextremeExamples.Models
+{
+    public class SchedulerDto
+    {
+        public string text { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string location { get; set; }
+        public string phone { get; set; }
+        public string description { get; set; }
+    }
+}
